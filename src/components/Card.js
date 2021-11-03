1debac86b609a547260bb30efb8349d2f692b0c9
maxrabin <max.rabin17@gmail.com>
2021-11-03 17:31:07 -0400

    basic structure for card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ item: { id, title, body, image } }) => {
     return (
         <div>
-            
+            <div>
+                <h2>{title}</h2>
+                <p>{body}</p>
+            </div>
+
+            <div>
+                <img src={`./images/${image}`}/>
+            </div>
         </div>
     )
 }
