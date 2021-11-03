2a3553cb41913b8799ae5dd2f353cba1b46fb460
maxrabin <max.rabin17@gmail.com>
2021-11-03 18:02:18 -0400

    Fixed some minor things

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import { FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa'
+import { FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { StyledSocialIcons } from './styles/SocialIcons.style';
 
 const SocialIcons = () => {
     return (
-        <div>
+        <StyledSocialIcons>
             <li>
                 <a href='https://twitter.com'>
                     <FaTwitter />
                 </a>
+            </li>
+            <li>
                 <a href='https://facebook.com'>
                     <FaFacebook />
                 </a>
+            </li>
+            <li>
                 <a href='https://linkedin.com'>
                     <FaLinkedin />
                 </a>
             </li>
-        </div>
+        </StyledSocialIcons>
     )
 }
 
